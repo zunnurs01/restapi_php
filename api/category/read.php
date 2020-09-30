8f93358ec6984007fc123497e468c25c84dd2ee1
DESKTOP-94CJTGJ\User <zunnur.brad@gmail.com>
2020-09-30 10:04:57 +0800

    move category folder into api folder

--- api/category/read.php
+++ api/category/read.php
@@ -4,8 +4,8 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 // include database and object files
-include_once '../api/config/database.php';
-include_once '../api/objects/category.php';
+include_once '../config/database.php';
+include_once '../objects/category.php';
 
 // instantiate database and category object
 $database = new Database();
